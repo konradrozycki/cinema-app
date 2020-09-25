3a809fbd6923131ad15ac90db09f592f8e0f9b62
Konrad <konradrozycki0@gmail.com>
2020-09-25 21:16:57 +0200

    update

--- Startup/db.js
+++ Startup/db.js
@@ -4,13 +4,13 @@ const db = require("../configuration/index");
 mongoose.Promise = global.Promise;
 
 module.exports = function () {
-  mongoose
-    .connect(process.env.MONGODB_URI || db.db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("connected to MongoDB");
-      winston.info("connected to the mongodb .....");
-    });
+    mongoose
+        .connect(process.env.MONGODB_URI || db.db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("connected to MongoDB");
+            winston.info("connected to the MongoDB .....");
+        });
 };
